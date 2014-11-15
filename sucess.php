624fa5c50d4dae3cea1f1c7b20f8f99ae2b45a3c
Katrina Sanford <blueyed_qt@hotmail.com>
2014-11-15 13:41:52 -0800

    Second commit.  Have all features working except bonus

--- sucess.php
+++ sucess.php
@@ -1,5 +1,6 @@
 <?php 
 session_start();
+require_once('connection.php');
 ?>
 <!doctype html>
 <html lang="en">
@@ -9,11 +10,22 @@ session_start();
 	<link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
-	<?php if(!empty($_SESSION['message'])) {
-		echo "<p class='sucess'>" . $_SESSION['message'] . "</p>";
-		unset($_SESSION['message']);
-	}
-	?>
+<?php 
+	if(!empty($_SESSION['message'])) { ?>
+	<p class='sucess'><?= $_SESSION['message'] ?></p>
+<?php 	unset($_SESSION['message']); } ?>
+	<h1>Email Addresses Entered:</h1>
+	<form action="process.php" method="post">
+<?php 
+	$query2 = "SELECT id, email, DATE_FORMAT(created_at, '%b %e, %Y %I:%i %p') AS dc FROM emails";
+	$emails = fetch_all($query2);
+	foreach($emails as $email) { ?>
+	<h4><?= $email['email'] ?></h4> <h4><?= $email['dc'] ?></h4>
+	<input type="hidden" name="<?= $email['id'] ?>" value=""/>
+	<input type="submit" name="delete" value="Delete!"/>
+<?php 
+	} ?>
+	</form>
 </body>
 </html>
 <?php session_destroy(); ?>
