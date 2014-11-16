7f7f728603d7c7347880a82a0ac9540ffae3fd0e
Katrina Sanford <blueyed_qt@hotmail.com>
2014-11-15 17:01:55 -0800

    Updating master

--- process.php
+++ process.php
@@ -8,19 +8,21 @@ if(!empty($_POST['email'])) {
 	$_SESSION['message'] = '';
 	if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 		$_SESSION['message'] = 'The email address ' . $email . ' is NOT a valid email address!';
-		// header('location: index.php');
+		header('location: index.php');
+		exit();
 	} else {
 		$query = "INSERT INTO emails (email, created_at) VALUES ('$email', NOW())";
 		$results1 = run_mysql_query($query);
 		if($results1) {
 			$_SESSION['message'] = 'The email address '. $email . ' is a VALID email address!  Thank you!';
-			// header('location: sucess.php');
+			header('location: sucess.php');
 		} else {
 			$SESSION['message'] = 'There was an error adding your email, please try again.';
-			// header('location: index.php');
+			header('location: index.php');
 		}
 	}
-} elseif (!empty($_POST['delete'])) {
-	 echo $_POST;
-}
+} 
+// elseif (!empty($_POST['delete'])) {
+// 	 echo $_POST;
+// }
 ?>
